e945161a182f56887fa4a1a2c2514d7cdb4eb430
sunny park <sunnyparkk@gmail.com>
2022-07-05 11:12:30 -0400

    create geometry object and add resize function

--- main.js
+++ main.js
@@ -1,9 +1,40 @@
-const scene = new THREE.Scene();
+let scene, camera, renderer, cube;
+function init() {
+	scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer();
+	renderer = new THREE.WebGLRenderer({ antialias: true });
 
-renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setSize(window.innerWidth, window.innerHeight);
 
-document.body.appendChild(renderer.domElement);
+	document.body.appendChild(renderer.domElement);
+
+	const geometry = new THREE.BoxGeometry(2, 2, 2);
+	const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+	cube = new THREE.Mesh(geometry, material);
+	scene.add(cube);
+
+	camera.position.z = 5;
+}
+
+function animate() {
+	requestAnimationFrame(animate);
+
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+
+	renderer.render(scene, camera);
+}
+
+function onWindowResize() {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener("resize", onWindowResize, false);
+
+init();
+animate();
